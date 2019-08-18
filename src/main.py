19d39866ace4c15a957ceef69c18a850112e18c1
cojae <co.jae@protonmail.com>
2019-08-18 16:51:32 -0400

    Testing out camera functionality, created a picture displayer application, previews for a second before dying

--- src/main.py
+++ src/main.py
@@ -1,1 +1,15 @@
-print "hello World"
+import picamera
+from time import sleep
+
+#Test camera functionality
+camera = picamera.PiCamera()
+camera.vflip = False
+camera.hflip = False
+camera.brightness = 60
+
+camera.start_preview()
+sleep(1.0)
+camera.stop_preview()
+
+
+print("Hello World")
