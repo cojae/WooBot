de08fc8b4d0841d6016d44a6b9383fb48ec432d1
cojae <co.jae@protonmail.com>
2019-08-29 19:10:47 -0400

    using cv2 now, just to look at image

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import picamera
+import picamera.array
 from time import sleep
 
 import io
@@ -6,14 +7,19 @@ import cv2
 import numpy
 
 #Test camera functionality
-camera = picamera.PiCamera()
-camera.vflip = False
-camera.hflip = False
-camera.brightness = 60
+with picamera.PiCamera() as camera :
+    camera.resolution = (640,480)
+    camera.vflip = False
+    camera.hflip = False
+    camera.brightness = 60
+    with picamera.array.PiRGBArray(camera) as rawCapture :
+        sleep(1.0) #Camera WarmUp
+        camera.capture(rawCapture, format="bgr")
+        image = rawCapture.array
 
-camera.start_preview()
-sleep(10.0)
-camera.stop_preview()
+        # Display the image on screen and wait for a keypress
+        cv2.imshow("Image", image)
+        cv2.waitKey(0)
 
 
 print("Hello World")
